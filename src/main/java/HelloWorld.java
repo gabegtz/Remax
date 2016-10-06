51ff2cfdbb234ede4eb841e9cb0609e7a21e41ee
Gabriel Gutierrez <Gabriel Gutierrez>
2016-10-05 22:02:58 -0500

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Gabriel Gutierrez on 10/4/2016.
+ */
+public class HelloWorld extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
