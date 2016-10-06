eb35968ad8655de83c90442844396f47085dc955
Gabriel Gutierrez <gegr93@gmail.com>
2016-10-05 22:33:11 -0500

    MainController with comments for screen deployment

---
+++ src/main/java/MainController.java
@@ -0,0 +1,17 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Gabriel Gutierrez on 10/5/2016.
+ */
+public class MainController implements Initializable {
+
+    public String text;
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
